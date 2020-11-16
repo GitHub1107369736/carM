05ad5578c1851b2bcd81fd23e5e003f6c874d741
xk <1107369736@qq.com>
2020-11-16 09:38:14 +0800

    项目包

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: {
+
+        },
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+
+        },
+        components: {
+
+        },
+        mounted(){
+          // this.$router.push('/');
+        }
+    };
+</script>
+
+<style lang="less">
+#app {
+    height: 100%;
+    
+}
+
+
+</style>
---
+++ src/components/begin/top.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="top">
+        <h1>嘻嘻哈哈或</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+
+        },
+        data() {
+            return {
+
+            };
+        },
+        methods: {
+
+        },
+        components: {
+
+        },
+    };
+</script>
+
+<style scoped lang="less">
+    .top {
+        height: 100%;
+        background-color: rgb(238,238,238);
+    }
+
+</style>
